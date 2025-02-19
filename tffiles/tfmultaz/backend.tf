c037a6c3877b6ec5b3e9b33357cad7352e8bef24
lockonstratos216 <vedanttandel216@gmail.com>
2025-02-19 17:35:59 +0530

    Update backend.tf

--- tffiles/tfmultaz/backend.tf
+++ tffiles/tfmultaz/backend.tf
@@ -3,6 +3,6 @@ terraform {
     storage_account_name = "d16"
     container_name = "dreamy"
     key = "terraform.tfstate"
-    access_key = "gAmL2E6zPzzrMN8mBKDaRN+Of8wkmOPrhClFVaaH3KqWrM0jaK4jaeO+ueaH27koS5Jdkifp30Hw+AStAEI1fQ=="
+    access_key = ""
   }
 }
